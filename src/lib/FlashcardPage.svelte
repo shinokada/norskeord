9295fc205790beccdd42dc0218711984d2c45014
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2023-12-30 10:01:56 +0100

    style: add opacity to buttons

--- src/lib/FlashcardPage.svelte
+++ src/lib/FlashcardPage.svelte
@@ -1,6 +1,6 @@
 <script>
 	// import dictionary from '$lib/data/verbs.json';
-
+  import { twMerge } from 'tailwind-merge';
 	import { Flashcard, ArrowLeft, ArrowRight } from '$lib';
   import { getRandomPair } from '$lib/utils.svelte.js';
   let { dictionary, title="Flashcard", pFront, pBack } = $props()
@@ -10,6 +10,8 @@
 	let showCardBack = $state(false)	
 	let showFront = $state('Vis norsk')
 	let showBack = $state('Show English')
+	let lang1lang2 = $state("text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800 opacity-100");
+	let lang2lang1 = $state("focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 opacity-50");
 
 	const toggleShowBack = () => showCardBack = !showCardBack;
 
@@ -18,9 +20,13 @@
 		if (lang === 'noreng'){
 			showFront = 'Vis norsk'
 			showBack = 'Show English'
+			lang1lang2 = twMerge(lang1lang2, 'opacity-100')
+			lang2lang1 = twMerge(lang2lang1, 'opacity-50')
 		} else if (lang === 'engnor'){
       showFront = 'Show English'
 			showBack = 'Vis norsk'
+			lang1lang2 = twMerge(lang1lang2, 'opacity-50')
+			lang2lang1 = twMerge(lang2lang1, 'opacity-100')
 		}
 		showCardBack = false
     const { front: newFront, back: newBack } = getRandomPair(dictionary, lang);
@@ -52,8 +58,8 @@
 <div class="flex flex-col items-center mt-15">
 	<h1 class="text-3xl m-4">{title}</h1>
 	<div class="flex justify-between">
-		<button type="button" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800" on:click={() => updateLang('noreng')}>Norsk-English</button>
-		<button class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" on:click={() => updateLang('engnor')}>English-Norsk</button>
+		<button type="button" class="{lang1lang2}" on:click={() => updateLang('noreng')}>Norsk-English</button>
+		<button class="{lang2lang1}" on:click={() => updateLang('engnor')}>English-Norsk</button>
 	</div>
 	<!-- FLASHCARD -->
 	<div class="bg-transparent w-full md:w-2/3 h-96">
--- src/lib/FlashcardPageExtended.svelte
+++ src/lib/FlashcardPageExtended.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { twMerge } from 'tailwind-merge';
 	import { Flashcard, ArrowLeft, ArrowRight } from '$lib';
   import { getRandomPair } from '$lib/utils.svelte.js';
   let { dictionary, title="Flashcard", pFront, pBack } = $props()
@@ -9,6 +10,9 @@
 	let showCardBack = $state(false)	
 	let showFront = $state('Vis norsk')
 	let showBack = $state('Show English')
+	let lang1lang2 = $state("text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800 opacity-100");
+	let lang2lang1 = $state("focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 opacity-50");
+	let lang1lang1 = $state("focus:outline-none text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-900 opacity-50")
 
 	const toggleShowBack = () => showCardBack = !showCardBack;
 
@@ -18,12 +22,21 @@
 		if (lang === 'noreng'){
 			showFront = 'Vis norsk'
 			showBack = 'Show English'
+			lang1lang2 = twMerge(lang1lang2, 'opacity-100')
+			lang2lang1 = twMerge(lang2lang1, 'opacity-50')
+			lang1lang1 = twMerge(lang1lang1, 'opacity-50')
 		} else if (lang === 'engnor'){
       showFront = 'Show English'
 			showBack = 'Vis norsk'
+			lang1lang2 = twMerge(lang1lang2, 'opacity-50')
+			lang2lang1 = twMerge(lang2lang1, 'opacity-100')
+			lang1lang1 = twMerge(lang1lang1, 'opacity-50')
 		} else if ( lang === 'nornor'){
 			showFront = 'Forklaring'
 			showBack = 'Vis norsk'
+			lang1lang2 = twMerge(lang1lang2, 'opacity-50')
+			lang2lang1 = twMerge(lang2lang1, 'opacity-50')
+			lang1lang1 = twMerge(lang1lang1, 'opacity-100')
 		}
 		showCardBack = false
     const { front: newFront, back: newBack, norskexplanation } = getRandomPair(dictionary, lang, true);
@@ -58,9 +71,9 @@
 <div class="flex flex-col items-center mt-15">
 	<h1 class="text-3xl m-4">{title}</h1>
 	<div class="flex justify-between">
-		<button type="button" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800" on:click={() => updateLang('noreng')}>Norsk-English</button>
-		<button class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" on:click={() => updateLang('engnor')}>English-Norsk</button>
-		<button class="focus:outline-none text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-900" on:click={() => updateLang('nornor')}>Norsk-Norsk</button>
+		<button type="button" class="{lang1lang2}" on:click={() => updateLang('noreng')}>Norsk-English</button>
+		<button class="{lang2lang1}" on:click={() => updateLang('engnor')}>English-Norsk</button>
+		<button class="{lang1lang1}" on:click={() => updateLang('nornor')}>Norsk-Norsk</button>
 	</div>
 	<!-- FLASHCARD -->
 	<div class="bg-transparent w-full md:w-2/3 h-96">
