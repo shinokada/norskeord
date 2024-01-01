82356cdbabe078e16ebe7169ed333c7a28bc8154
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-01 21:28:22 +0100

    fix: go back getRandomPair since it is returns error often

--- src/lib/utils.svelte.js
+++ src/lib/utils.svelte.js
@@ -18,84 +18,42 @@ export function getRandomItemFromDictionary (dictionary) {
 //   return { norsk, english };
 // }
 
-
-// Shuffle function
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
-// Initialize shuffled indices and used indices
-let shuffledIndices = [];
-let usedIndices = new Set();
-
 export function getRandomPair(jsonData, langlang, isExplain = false) {
-
-  if (shuffledIndices.length === 0) {
-    // All indices have been used, reshuffle
-    shuffledIndices = shuffleArray(Array.from(Array(jsonData.length).keys()));
-    usedIndices.clear(); // Clear used indices
-  }
-
-  let randomIndex = shuffledIndices.pop();
-  while (usedIndices.has(randomIndex)) {
-    // If the index has been used, get a new one
-    randomIndex = shuffledIndices.pop();
-  }
-
-  usedIndices.add(randomIndex);
-
+  const randomIndex = Math.floor(Math.random() * jsonData.length);
   const randomPair = jsonData[randomIndex];
-
-  let front, back, norskexplanation;
-
+  // console.log('randomPair', randomPair)
+  let front
+  let back
+  
   if (isExplain) {
-    if (
-      randomPair &&
-      ('norsk' in randomPair) &&
-      ('english' in randomPair) &&
-      ('norskexplanation' in randomPair)
-    ) {
-      const { norsk, english, norskexplanation } = randomPair;
+    let { norsk, english, norskexplanation } = randomPair;
 
-      if (langlang === 'noreng') {
-        front = norsk;
-        back = english;
-      } else if (langlang === 'engnor') {
-        front = english;
-        back = norsk;
-      } else if (langlang === 'nornor') {
-        front = norskexplanation;
-        back = norsk;
-      }
-    } else {
-      throw new Error('Invalid data structure for explanation in JSON');
-      // Handle the error case by throwing an error or returning a default response
+    if (langlang === 'noreng') {
+      front = norsk;
+      back = english;
+    } else if (langlang === 'engnor') {
+      front = english;
+      back = norsk;
+    } else if (langlang === 'nornor') {
+      front = norskexplanation;
+      back = norsk;
     }
+
+    return { front, back, norskexplanation };
   } else {
-    if (randomPair && ('norsk' in randomPair) && ('english' in randomPair)) {
-      const { norsk, english } = randomPair;
-
-      if (langlang === 'noreng') {
-        front = norsk;
-        back = english;
-      } else if (langlang === 'engnor') {
-        front = english;
-        back = norsk;
-      }
-    } else {
-      throw new Error('Invalid data structure in JSON');
-      // Handle the error case by throwing an error or returning a default response
+    let { norsk, english } = randomPair;
+  
+    if (langlang === 'noreng') {
+      front = norsk;
+      back = english;
+    } else if (langlang === 'engnor') {
+      front = english;
+      back = norsk;
     }
+
+    return { front, back };
   }
-
-  return isExplain ? { front, back, norskexplanation } : { front, back };
 }
-
-
 
 
 export function openTab(word, website) {
