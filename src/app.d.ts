d1fa3b8dc0650b3122b19664544e5d094e7ffd62
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-02-18 07:49:37 +0100

    docs: update about page by adding githuburl

--- src/app.d.ts
+++ src/app.d.ts
@@ -9,7 +9,8 @@ declare global {
 		// interface Platform {}
 	}
 	declare const __NAME__: string;
-  declare const __VERSION__: string;
+	declare const __VERSION__: string;
+	declare const __GITHUBURL__: string;
   declare const __SVELTE5UILIBVERSION__: string;
   declare const __SVELTEVERSION__: string;
   declare const __SVELTEKITVERSION__: string;
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,6 +2,7 @@
 import { removeHyphensAndCapitalize } from '$lib/utils.svelte';
 const name = __NAME__;
 const version = __VERSION__;
+const githuburl = __GITHUBURL__;
 const svelte5uilibVersion = __SVELTE5UILIBVERSION__;
 const svelteVersion = __SVELTEVERSION__;
 const svelteKitVersion = __SVELTEKITVERSION__;
@@ -75,7 +76,7 @@ const viteVersion = __VITEVERSION__;
   <ul class="m-4 list-disc p-4 text-left text-lg dark:text-gray-400">
     <li class="hover:text-red-700 hover:underline">
       <a
-        href="https://github.com/shinokada/norske-flashcard"
+        href="{githuburl}"
         class="me-4 hover:underline md:me-6">Github repo</a
       >
     </li>
