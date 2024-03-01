00c9a8216a3bf28397a31743d8202a918658073f
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-03-01 21:20:46 +0100

    test: change credits to about

--- tests/test.ts
+++ tests/test.ts
@@ -30,7 +30,7 @@ test('education page has expected h1', async ({ page }) => {
 	expect(await page.textContent('h1')).toBe('Grunnopplæringen');
 });
 
-test('credits page has expected h1', async ({ page }) => {
-	await page.goto('/credits');
-	expect(await page.textContent('h1')).toBe('Credits');
+test('about page has expected h1', async ({ page }) => {
+	await page.goto('/about');
+	expect(await page.textContent('h1')).toBe('About');
 });
