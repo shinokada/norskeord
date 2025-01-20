1ef590edcc499f7f2a3c067944aca165d0783275
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-01-20 07:28:44 +0100

    feat: add saying page

--- src/routes/components/Nav.svelte
+++ src/routes/components/Nav.svelte
@@ -1,18 +1,27 @@
 <script>
-	import { Navbar, NavLi, NavBrand, NavUl, uiHelpers, Darkmode } from 'flowbite-svelte';
+	import { Navbar, NavLi, NavBrand, NavUl, uiHelpers, Darkmode, Dropdown, DropdownUl, DropdownLi } from 'flowbite-svelte';
 	import No from '$lib/No.svelte';
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
+	import ChevronDownOutline from "./ChevronDownOutline.svelte";
 
-	let activeUrl = $state($page.url.pathname);
-	$effect(() => {
-		activeUrl = $page.url.pathname;
-	});
+	let activeUrl = $state(page.url.pathname);
+
+	// for Dropdown
+  let dropdown = uiHelpers();
+  let dropdownStatus = $state(false);
+	let toggleDropdown = dropdown.toggle;
+  let closeDropdown = dropdown.close;
+	// for Dropdonw2
+	let dropdown2 = uiHelpers();
+	let dropdown2Status = $state(false);
+	let toggleDropdown2 = dropdown2.toggle;
+	let closeDropdown2 = dropdown2.close;
 
 	let nav = uiHelpers();
-
 	let navStatus = $state(false);
 	let toggleNav = nav.toggle;
 	let closeNav = nav.close;
+
 	let divClass = 'ml-auto w-full';
 	let ulclass =
 		'flex flex-col py-3 lg:flex-row lg:my-0 order-1 font-medium dark:lg:bg-transparent lg:bg-white lg:border-0';
@@ -23,6 +32,10 @@
 		// this can be done adding nav.navStatus directly to DOM element
 		// without using effect
 		navStatus = nav.isOpen;
+		dropdownStatus = dropdown.isOpen;
+		dropdown2Status = dropdown2.isOpen;
+		activeUrl = page.url.pathname;
+		$inspect('dropdown2Status', dropdown2Status);
 	});
 </script>
 
@@ -39,12 +52,19 @@
 			</div>
 		{/snippet}
 		<NavUl {activeUrl} class={ulclass}>
-			<NavLi href="/">Nivå A1</NavLi>
-			<NavLi href="/level-a2">Nivå A2</NavLi>
-			<NavLi href="/verbs">Verbs</NavLi>
-			<NavLi href="/adjectives">Adjectives</NavLi>
-			<NavLi href="/vocab">Vocab</NavLi>
-			<NavLi href="/education">Education</NavLi>
+			
+			<NavLi onclick={toggleDropdown2} class="cursor-pointer relative">
+        Vocab<ChevronDownOutline class="ms-2 inline h-6 w-6 text-primary-800 dark:text-white" />
+				<Dropdown dropdownStatus={dropdown2Status} closeDropdown={closeDropdown2} class="absolute -top-[20px] left-[100px] md:-left-[50px] md:top-[20px]">
+          <DropdownUl>
+            <DropdownLi href="/verbs">Verbs</DropdownLi>
+            <DropdownLi href="/adjectives">Adjectives</DropdownLi>
+            <DropdownLi href="/vocab">General</DropdownLi>
+						<DropdownLi href="/education">Education</DropdownLi>
+          </DropdownUl>
+        </Dropdown>
+      </NavLi>
+			<NavLi href="/sayings">Sayings</NavLi>
 			<NavLi href="/about">About</NavLi>
 		</NavUl>
 	</Navbar>
---
+++ src/routes/sayings/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { FlashcardPage } from '$lib';
+	import dictionary from '$lib/data/idioms.json';
+</script>
+
+<FlashcardPage {dictionary} title="Sayings" />
