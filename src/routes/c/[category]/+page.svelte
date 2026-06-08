7f096852ac2470a97eeb4a7c6594418deed121c1
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2026-06-08 07:22:34 +0200

    feat: combine c1 and c2 to c

---
+++ src/routes/c/[category]/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { VocabFlashcardPage } from '$lib';
+  import { removeHyphensAndCapitalize } from '$lib/utils';
+
+  let { data } = $props();
+
+  let categoryName = $derived(removeHyphensAndCapitalize(data.category));
+  const learningResourceSchemaJson = $derived(JSON.stringify(data.learningResourceSchema));
+</script>
+
+<svelte:head>
+  <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+  {@html '<scr' + 'ipt type="application/ld+json">' + learningResourceSchemaJson + '</scr' + 'ipt>'}
+</svelte:head>
+
+{#if data.entries.length > 0}
+  <VocabFlashcardPage
+    entries={data.entries}
+    title={categoryName}
+    level={data.level}
+    prevCategory={data.prevCategory}
+    nextCategory={data.nextCategory}
+  />
+{:else}
+  <div class="py-16 text-center">
+    <h1 class="text-2xl font-semibold text-gray-700 dark:text-gray-300">{categoryName}</h1>
+    <p class="mt-4 text-gray-500 dark:text-gray-400">
+      No vocabulary available yet. Check back soon!
+    </p>
+  </div>
+{/if}
