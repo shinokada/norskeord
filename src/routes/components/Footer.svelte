0d846d42aa13fa7a7f4b2902b155a3c37ee0ed03
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2026-06-18 18:20:54 +0200

    style: home, login, footer, plus update

--- src/routes/components/Footer.svelte
+++ src/routes/components/Footer.svelte
@@ -9,6 +9,6 @@
 <Footer class="shadow-none dark:bg-blue-950" footerType="logo">
   <div class="sm:flex sm:items-center sm:justify-between">
     <FooterBrand href="https://norskeord.no" name="Norskeord" />
-    <p class="mt-2 text-xs text-gray-400 sm:mt-0 dark:text-gray-500">v{__VERSION__}</p>
+    <p class="mt-2 text-sm text-gray-400 sm:mt-0 dark:text-gray-500">v{__VERSION__}</p>
   </div>
 </Footer>
